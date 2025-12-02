e9b3140b6cbbe10fdcf129dd0592b0735ff91b7e
Chris Daly <chrisdalyenq@gmail.com>
2025-12-02 11:53:44 -0800

    Add a placeholder player sprite

--- ContentLibrary/Paths.cs
+++ ContentLibrary/Paths.cs
@@ -5,6 +5,6 @@ public static class Paths
     public static class Images
     {
         public const string MonoGameLogo = @"Images\logo";
+        public const string Player = @"Images\Placeholder-Player.png";
     } 
-    
 }
