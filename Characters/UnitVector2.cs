786215f630a954adcafee1c55d334caa22ed4fbd
Chris Daly <chrisdalyenq@gmail.com>
2025-12-02 13:48:04 -0800

    Handle NaN in UnitVector2

--- Characters/UnitVector2.cs
+++ Characters/UnitVector2.cs
@@ -9,8 +9,8 @@ public readonly record struct UnitVector2
     {
         var v = new Vector2(x, y);
         v.Normalize();
-        X = v.X;
-        Y = v.Y;
+        X = float.IsNaN(v.X) ? 0f : v.X;
+        Y = float.IsNaN(v.Y) ? 0f : v.Y;
     }
     
     public static explicit operator Vector2(UnitVector2 uv) => new(uv.X, uv.Y);
