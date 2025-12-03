a493f2a1d3504330043439564c8a30555c7b8607
Chris Daly <chrisdalyenq@gmail.com>
2025-12-02 17:04:49 -0800

    Add gamepad support

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -12,8 +12,9 @@ public class InputManager(PlayerCharacter player)
     public void Update()
     {
         var keyboardState = Keyboard.GetState();
+        var gamePadState = GamePad.GetState(0);
 
-        if (keyboardState.IsKeyDown(Keys.Escape))
+        if (keyboardState.IsKeyDown(Keys.Escape) || gamePadState.Buttons.Back == ButtonState.Pressed)
         {
             OnExit();
             return;
@@ -22,10 +23,19 @@ public class InputManager(PlayerCharacter player)
         var x = 0f;
         var y = 0f;
 
+        // Keyboard input
         if (keyboardState.IsKeyDown(Keys.S)) x -= 1f;
         if (keyboardState.IsKeyDown(Keys.F)) x += 1f;
         if (keyboardState.IsKeyDown(Keys.E)) y -= 1f;
         if (keyboardState.IsKeyDown(Keys.D)) y += 1f;
+
+        // GamePad input (left thumbstick)
+        if (gamePadState.IsConnected)
+        {
+            var thumbStick = gamePadState.ThumbSticks.Left;
+            x += thumbStick.X;
+            y -= thumbStick.Y; // Y is inverted on thumbsticks
+        }
 
         if (x != 0f || y != 0f)
             player.DirectionInput(new UnitVector2(x, y));
