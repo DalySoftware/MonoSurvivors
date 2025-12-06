7589b5a42310ce65843d97cacc29088fdd4b2219
Chris Daly <chrisdalyenq@gmail.com>
2025-12-05 23:11:41 -0800

    Fix calculation bug
    Would have caused an issue if framerate < 1hz

--- Gameplay/Entities/MovableEntity.cs
+++ Gameplay/Entities/MovableEntity.cs
@@ -6,7 +6,9 @@ public abstract class MovableEntity(Vector2 position) : IEntity, IHasPosition
 {
     protected Vector2 Velocity { get; set; } = Vector2.Zero;
 
-    public virtual void Update(GameTime gameTime) => Position += Velocity * gameTime.ElapsedGameTime.Milliseconds;
+    public virtual void Update(GameTime gameTime) =>
+        Position += Velocity * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
     public bool MarkedForDeletion { get; protected set; }
     public Vector2 Position { get; private set; } = position;
 }
