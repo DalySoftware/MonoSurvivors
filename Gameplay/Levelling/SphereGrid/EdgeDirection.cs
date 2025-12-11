f8eb3dfe048fc7f39cfdd1900712759cc73c9d3e
Chris Daly <chrisdalyenq@gmail.com>
2025-12-10 19:15:45 -0800

    Small tidy ups

---
+++ Gameplay/Levelling/SphereGrid/EdgeDirection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Gameplay.Levelling.SphereGrid;
+
+public enum EdgeDirection
+{
+    TopLeft,
+    TopRight,
+    MiddleLeft,
+    MiddleRight,
+    BottomLeft,
+    BottomRight
+}
+
+public static class EdgeDirectionExtensions
+{
+    public static EdgeDirection Opposite(this EdgeDirection direction) => direction switch
+    {
+        EdgeDirection.TopLeft => EdgeDirection.BottomRight,
+        EdgeDirection.TopRight => EdgeDirection.BottomLeft,
+        EdgeDirection.MiddleLeft => EdgeDirection.MiddleRight,
+        EdgeDirection.MiddleRight => EdgeDirection.MiddleLeft,
+        EdgeDirection.BottomLeft => EdgeDirection.TopRight,
+        EdgeDirection.BottomRight => EdgeDirection.TopLeft,
+        _ => throw new ArgumentOutOfRangeException(nameof(direction))
+    };
+}
--- Gameplay/Levelling/SphereGrid/SphereGrid.cs
+++ Gameplay/Levelling/SphereGrid/SphereGrid.cs
@@ -69,6 +69,7 @@ public class SphereGrid
         }
     }
 
+    // ReSharper disable IdentifierTypo
     public static SphereGrid Create(Action<IPowerUp> onUnlock)
     {
         // Damage (right)
@@ -121,28 +122,5 @@ public class SphereGrid
             OnUnlock = onUnlock
         };
     }
-}
-
-public enum EdgeDirection
-{
-    TopLeft,
-    TopRight,
-    MiddleLeft,
-    MiddleRight,
-    BottomLeft,
-    BottomRight
-}
-
-public static class EdgeDirectionExtensions
-{
-    public static EdgeDirection Opposite(this EdgeDirection direction) => direction switch
-    {
-        EdgeDirection.TopLeft => EdgeDirection.BottomRight,
-        EdgeDirection.TopRight => EdgeDirection.BottomLeft,
-        EdgeDirection.MiddleLeft => EdgeDirection.MiddleRight,
-        EdgeDirection.MiddleRight => EdgeDirection.MiddleLeft,
-        EdgeDirection.BottomLeft => EdgeDirection.TopRight,
-        EdgeDirection.BottomRight => EdgeDirection.TopLeft,
-        _ => throw new ArgumentOutOfRangeException(nameof(direction))
-    };
+    // ReSharper enable IdentifierTypo
 }
