e33c5e94528cf28e051b84d4109e02af63af28d3
Chris Daly <chrisdalyenq@gmail.com>
2025-12-15 18:45:21 -0800

    Refactor: add constant

--- Gameplay/Combat/EnemyDeathBlast.cs
+++ Gameplay/Combat/EnemyDeathBlast.cs
@@ -7,11 +7,13 @@ namespace Gameplay.Combat;
 
 public static class EnemyDeathBlast
 {
+    private const float BaseDamage = 4f;
+
     public static void Explode(EntityManager entityManager, PlayerCharacter owner, Vector2 position, int bullets,
         float damageMultiplier)
     {
         var bulletDirections = ArcSpreader.EvenlySpace(ArcSpreader.RandomDirection(), bullets, MathF.Tau);
-        var damage = 4f * damageMultiplier;
+        var damage = BaseDamage * damageMultiplier;
 
         foreach (var direction in bulletDirections)
         {
