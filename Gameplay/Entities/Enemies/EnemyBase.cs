14e94738d728723a56d32c02c7f2b4db258d3611
Chris Daly <chrisdalyenq@gmail.com>
2025-12-15 17:03:36 -0800

    Fix multi-kill bug

--- Gameplay/Entities/Enemies/EnemyBase.cs
+++ Gameplay/Entities/Enemies/EnemyBase.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading;
 using Gameplay.CollisionDetection;
 using Gameplay.Combat;
 using Gameplay.Levelling;
@@ -8,6 +9,8 @@ namespace Gameplay.Entities.Enemies;
 public abstract class EnemyBase(Vector2 position, int damage)
     : MovableEntity(position), IDamagesPlayer, ICreatesExperienceOnDeath
 {
+    private int _isDead; // Marker for concurrency
+    
     internal IEnumerable<EnemyBase> NearbyEnemies { get; set; } = [];
 
     public float Health { get; protected set; }
@@ -16,11 +19,13 @@ public abstract class EnemyBase(Vector2 position, int damage)
 
     public int Damage { get; } = damage;
     public required ICollider Collider { get; init; }
-
     public void TakeDamage(PlayerCharacter damager, float amount)
     {
         Health -= amount;
         if (Health > 0) return;
+
+        if (Interlocked.Exchange(ref _isDead, 1) == 1)
+            return; // another thread already processed death
 
         MarkedForDeletion = true;
         damager.OnKill(this);
