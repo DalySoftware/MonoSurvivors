cd00c5779d572dc34bd0151e289d1d2c941afb6b
Chris Daly <chrisdalyenq@gmail.com>
2025-12-15 19:12:51 -0800

    Refactor: extract GunBase

--- Gameplay/Combat/Weapons/Projectile/BasicGun.cs
+++ Gameplay/Combat/Weapons/Projectile/BasicGun.cs
@@ -5,46 +5,22 @@ using Gameplay.Entities;
 namespace Gameplay.Combat.Weapons.Projectile;
 
 public class BasicGun(PlayerCharacter owner, ISpawnEntity spawnEntity, IEntityFinder entityFinder, IAudioPlayer audio)
-    : IWeapon
+    : GunBase(owner.WeaponBelt.Stats)
 {
-    private const float BulletSpeed = 1f;
-    private readonly TimeSpan _cooldown = TimeSpan.FromSeconds(1);
-
     private readonly BulletSplitter _bulletSplitter = new(owner, MathF.PI / 6, 0.5f, spawnEntity);
-    private readonly ExtraShotHandler _extraShotHandler = new(owner.WeaponBelt.Stats);
 
-    private WeaponBeltStats _stats = new();
-
-    private TimeSpan _remainingCooldown = TimeSpan.Zero;
-
-    public void Update(GameTime gameTime, WeaponBeltStats stats)
-    {
-        _stats = stats;
-
-        // Handle extra shots first
-        var extraShotsResult = _extraShotHandler.Update(gameTime, Shoot);
-        if (extraShotsResult is ExtraShotResult.WaitingToFire or ExtraShotResult.Fired)
-            return;
-
-        // Normal shooting
-        _remainingCooldown -= gameTime.ElapsedGameTime;
-        if (_remainingCooldown > TimeSpan.Zero) return;
-
-        Shoot();
-        _extraShotHandler.QueueFire();
-        _remainingCooldown = _cooldown / stats.AttackSpeedMultiplier;
-    }
-
-    private void Shoot()
+    protected override void Shoot()
     {
         var target = entityFinder.NearestEnemyTo(owner);
         if (target == null) return;
 
-        var baseDamage = 8f * _stats.DamageMultiplier;
-        var damage = CritCalculator.CalculateDamage(baseDamage, _stats.CritChance, _stats.CritDamage);
-        var range = 300f * _stats.RangeMultiplier;
-        var bullet = new Bullet(owner, owner.Position, target.Position, damage, range, _stats.Pierce,
-            BulletSpeed * _stats.SpeedMultiplier, _bulletSplitter.SpawnAnyOnHitBulletSplits);
+        const float bulletSpeed = 1f;
+        var baseDamage = 8f * Stats.DamageMultiplier;
+        var damage = CritCalculator.CalculateDamage(baseDamage, Stats.CritChance, Stats.CritDamage);
+        var range = 300f * Stats.RangeMultiplier;
+
+        var bullet = new Bullet(owner, owner.Position, target.Position, damage, range, Stats.Pierce,
+            bulletSpeed * Stats.SpeedMultiplier, _bulletSplitter.SpawnAnyOnHitBulletSplits);
         spawnEntity.Spawn(bullet);
         audio.Play(SoundEffectTypes.Shoot);
     }
---
+++ Gameplay/Combat/Weapons/Projectile/GunBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gameplay.Combat.Weapons.Projectile;
+
+public abstract class GunBase(WeaponBeltStats stats) : IWeapon
+{
+    private readonly TimeSpan _cooldown = TimeSpan.FromSeconds(1);
+    private readonly ExtraShotHandler _extraShotHandler = new(stats);
+    private TimeSpan _remainingCooldown = TimeSpan.Zero;
+    protected WeaponBeltStats Stats { get; private set; } = new();
+
+    public void Update(GameTime gameTime, WeaponBeltStats stats)
+    {
+        Stats = stats;
+
+        // Handle extra shots first
+        var extraShotsResult = _extraShotHandler.Update(gameTime, Shoot);
+        if (extraShotsResult is ExtraShotResult.WaitingToFire or ExtraShotResult.Fired)
+            return;
+
+        // Normal shooting
+        _remainingCooldown -= gameTime.ElapsedGameTime;
+        if (_remainingCooldown > TimeSpan.Zero) return;
+
+        Shoot();
+        _extraShotHandler.QueueFire();
+        _remainingCooldown = _cooldown / stats.AttackSpeedMultiplier;
+    }
+    protected abstract void Shoot();
+}
