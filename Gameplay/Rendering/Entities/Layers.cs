4061db927bb8ac955a813332b166de6439589e2d
Chris Daly <chrisdalyenq@gmail.com>
2025-12-18 16:39:54 -0800

    Layer pickups below enemies

--- Gameplay/Rendering/Entities/Layers.cs
+++ Gameplay/Rendering/Entities/Layers.cs
@@ -6,6 +6,6 @@ public static class Layers
     public const float Ui = 0.8f;
     public const float Projectiles = 0.75f;
     public const float Player = 0.7f;
-    public const float Pickups = 0.5f;
-    public const float Enemies = 0.4f;
+    public const float Enemies = 0.5f;
+    public const float Pickups = 0.4f;
 }
