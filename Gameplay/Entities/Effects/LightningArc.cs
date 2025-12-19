1f4ad72f53d9fa485f2522f79d65066b7d56199d
Chris Daly <chrisdalyenq@gmail.com>
2025-12-18 16:53:18 -0800

    Tiny bit of lightning visual sauce

--- Gameplay/Entities/Effects/LightningArc.cs
+++ Gameplay/Entities/Effects/LightningArc.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using Gameplay.Rendering;
+using Gameplay.Rendering.Colors;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Gameplay.Entities.Effects;
 
 internal sealed class LightningArc(Vector2 from, Vector2 to) : IEntity, IPrimitiveVisual
 {
-    private TimeSpan _lifetime = TimeSpan.FromMilliseconds(60);
+    private readonly static TimeSpan MaxLifetime = TimeSpan.FromMilliseconds(180);
+    private readonly Vector2 _jitter = new((Random.Shared.NextSingle() - 0.5f) * 12f,
+        (Random.Shared.NextSingle() - 0.5f) * 12f);
+    private TimeSpan _lifetime = MaxLifetime;
 
     public bool MarkedForDeletion { get; private set; }
 
@@ -16,9 +20,18 @@ internal sealed class LightningArc(Vector2 from, Vector2 to) : IEntity, IPrimiti
         if (_lifetime <= TimeSpan.Zero)
             MarkedForDeletion = true;
     }
+
     public Vector2 Position => (from + to) * 0.5f;
     public float Layer => Layers.Projectiles;
 
-    public void Draw(SpriteBatch spriteBatch, PrimitiveRenderer renderer) =>
-        renderer.DrawLine(spriteBatch, from, to, Color.Yellow, 10f, Layer);
+    public void Draw(SpriteBatch spriteBatch, PrimitiveRenderer renderer)
+    {
+        var t = (float)(_lifetime.TotalMilliseconds / MaxLifetime.TotalMilliseconds);
+        var thickness = MathHelper.Lerp(12f, 2f, 1f - t);
+        var baseColor = Color.DeepSkyBlue;
+
+        renderer.DrawLine(spriteBatch, from + _jitter, to + _jitter,
+            baseColor * 0.3f * t, thickness * 2f, Layer - 0.01f);
+        renderer.DrawLine(spriteBatch, from, to, baseColor.ShiftLightness(0.5f) * t, thickness, Layer);
+    }
 }
