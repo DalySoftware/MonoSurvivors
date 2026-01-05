b267060e4f85012fcd8b85242b6d1d63e4925844
Chris Daly <chrisdalyenq@gmail.com>
2026-01-05 15:44:18 -0800

    Remove unused method

--- GameLoop/DependencyInjection/ServiceConfiguration.cs
+++ GameLoop/DependencyInjection/ServiceConfiguration.cs
@@ -1,13 +1,6 @@
 using Autofac;
-using GameLoop.Audio;
 using GameLoop.Persistence;
 using GameLoop.UserSettings;
-using Gameplay.Audio;
-using Gameplay.Entities;
-using Gameplay.Rendering;
-using Gameplay.Rendering.Effects;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace GameLoop.DependencyInjection;
 
@@ -15,30 +8,6 @@ internal static class ServiceConfiguration
 {
     extension(ContainerBuilder builder)
     {
-        internal void ConfigureContentServices(GraphicsDevice graphicsDevice,
-            ContentManager content)
-        {
-            builder.RegisterInstance(graphicsDevice)
-                .SingleInstance();
-
-            builder.RegisterInstance(content)
-                .SingleInstance();
-
-            builder.RegisterType<PrimitiveRenderer>()
-                .SingleInstance();
-
-            builder.RegisterType<MusicPlayer>()
-                .SingleInstance();
-
-            builder.RegisterType<SoundEffectPlayer>().As<IAudioPlayer>();
-
-            builder.RegisterType<EffectManager>()
-                .SingleInstance();
-
-            builder.RegisterType<EntityManager>()
-                .SingleInstance();
-        }
-
         internal void ConfigureOptions()
         {
             builder.RegisterType<AppDataPersistence>()
