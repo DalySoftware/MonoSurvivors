183d5d5930b6a0c8fbe1c9fc099b4b999254ebc1
Chris Daly <chrisdalyenq@gmail.com>
2026-01-09 16:14:17 -0800

    Refactor game over screen

--- GameLoop/Scenes/GameOver/GameOverScene.cs
+++ GameLoop/Scenes/GameOver/GameOverScene.cs
@@ -1,58 +1,91 @@
 using Autofac;
 using ContentLibrary;
 using GameLoop.Input;
-using Gameplay.Rendering;
+using GameLoop.UI;
 using Gameplay.Rendering.Colors;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GameLoop.Scenes.GameOver;
 
-internal class GameOverScene(
-    ContentManager content,
-    SpriteBatch spriteBatch,
-    GameWindow window,
-    GameOverInputManager input,
-    InputGate inputGate,
-    GameInputState inputState)
-    : IScene
+internal class GameOverScene : IScene
 {
-    private readonly SpriteFont _messageFont = content.Load<SpriteFont>(Paths.Fonts.BoldPixels.Large);
-    private readonly SpriteFont _titleFont = content.Load<SpriteFont>(Paths.Fonts.Righteous.Large);
+    private readonly SpriteBatch _spriteBatch;
+    private readonly GameOverInputManager _input;
+    private readonly InputGate _inputGate;
+    private readonly GameInputState _inputState;
+    private readonly Label _titleLabel;
+    private readonly Label _instructionsLabel;
+    private InputMethod _lastInputMethod;
+
+    public GameOverScene(
+        Viewport viewport,
+        SpriteBatch spriteBatch,
+        GameOverInputManager input,
+        InputGate inputGate,
+        GameInputState inputState,
+        Label.Factory labelFactory)
+    {
+        _spriteBatch = spriteBatch;
+        _input = input;
+        _inputGate = inputGate;
+        _inputState = inputState;
+
+        const string titleText = "Game Over";
+        var titleSize = labelFactory.Measure(Paths.Fonts.Righteous.Large, titleText);
+        var titleRectangle = viewport.UiRectangle()
+            .CreateAnchoredRectangle(UiAnchor.Centre, titleSize, new Vector2(0f, -100f));
+
+        _titleLabel = labelFactory.Create(
+            Paths.Fonts.Righteous.Large,
+            "Game Over",
+            titleRectangle.Origin,
+            titleRectangle.OriginAnchor,
+            ColorPalette.Red,
+            TextAlignment.Center);
+
+        var instructionsText = GetInstructionsText();
+
+        _instructionsLabel = labelFactory.Create(
+            Paths.Fonts.BoldPixels.Large,
+            instructionsText,
+            titleRectangle.AnchorForPoint(UiAnchor.BottomCenter) + new Vector2(0f, 100f),
+            UiAnchor.TopCenter,
+            ColorPalette.LightGray,
+            TextAlignment.Center);
+    }
 
     public void Update(GameTime gameTime)
     {
-        if (inputGate.ShouldProcessInput())
-            input.Update();
+        if (_inputGate.ShouldProcessInput())
+            _input.Update();
+
+        // Update instructions text if input method changed
+        var currentInput = _inputState.CurrentInputMethod;
+        if (currentInput != _lastInputMethod)
+        {
+            _instructionsLabel.Text = GetInstructionsText();
+            _lastInputMethod = currentInput;
+        }
     }
 
     public void Draw(GameTime gameTime)
     {
-        spriteBatch.Begin(SpriteSortMode.FrontToBack);
+        _spriteBatch.Begin(SpriteSortMode.FrontToBack);
 
-        // Draw "Game Over" title
-        const string titleText = "Game Over";
-        var titleSize = _titleFont.MeasureString(titleText);
-        var titlePosition = new Vector2(
-            window.Centre.X - titleSize.X / 2,
-            window.Centre.Y - 100);
-        spriteBatch.DrawString(_titleFont, titleText, titlePosition, ColorPalette.Red);
+        _titleLabel.Draw(_spriteBatch);
+        _instructionsLabel.Draw(_spriteBatch);
 
-        // Draw instructions
-        var instructionsText = inputState.CurrentInputMethod is InputMethod.KeyboardMouse
-            ? "SPACE to Restart | ESC to Exit"
-            : "START to Restart | BACK to Exit";
-        var instructionsSize = _messageFont.MeasureString(instructionsText);
-        var instructionsPosition = new Vector2(
-            window.Centre.X - instructionsSize.X / 2,
-            window.Centre.Y + 50);
-        spriteBatch.DrawString(_messageFont, instructionsText, instructionsPosition, ColorPalette.LightGray);
-
-        spriteBatch.End();
+        _spriteBatch.End();
     }
 
-    public void Dispose() => spriteBatch.Dispose();
+    public void Dispose() { }
+
+
+    private string GetInstructionsText() =>
+        _inputState.CurrentInputMethod is InputMethod.KeyboardMouse
+            ? "SPACE to Restart | ESC to Exit"
+            : "START to Restart | BACK to Exit";
 
     public static void ConfigureServices(ContainerBuilder builder)
     {
